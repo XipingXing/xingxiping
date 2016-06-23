4c95450df40827d482554de30d1044798aa70e7e
XipingXing <xiping.xing@hotmail.com>
2016-06-23 14:51:24 +0800

    update by xingxiping

--- platform/platform-sell-user/src/main/java/org/platform/sell/user/dao/TestDao.java
+++ platform/platform-sell-user/src/main/java/org/platform/sell/user/dao/TestDao.java
@@ -3,6 +3,6 @@ package org.platform.sell.user.dao;
 import org.platform.sell.user.model.TestModel;
 import org.xxp.platform.common.core.hibernate.BaseDao;
 
-public interface TestDao extends BaseDao<TestModel>{
+public interface TestDao extends BaseDao<TestModel> {
   
 }
